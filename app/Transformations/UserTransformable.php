dbda82e27be44b904dea719018e888c94e9d1f03
Michael Hampton <michaelhamptondesign@yahoo.com>
2019-10-03 15:37:49 +0100

    display department name

--- app/Transformations/UserTransformable.php
+++ app/Transformations/UserTransformable.php
@@ -1,29 +1,36 @@
 <?php
+
 namespace App\Transformations;
 
 use App\User;
-use App\Repositories\UserRepository;
-use App\Customer;
+use App\Repositories\DepartmentRepository;
+use App\Department;
 
-trait UserTransformable
-{
+trait UserTransformable {
+
     /**
      * Transform the user
      *
      * @param User $user
      * @return User
      */
-    protected function transformUser(User $user)
-    {
+    protected function transformUser(User $user) {
         $prop = new User;
-        
+
         $prop->id = (int) $user->id;
         $prop->first_name = $user->first_name;
         $prop->last_name = $user->last_name;
         $prop->email = $user->email;
         $prop->username = $user->username;
         $prop->department = $user->departments()->pluck('department_id')->all();
-        
+        $prop->dept = '';
+
+        if ($user->departments->count() > 0) {
+            $objDepartment = $user->departments->first();
+            $prop->dept = $objDepartment->name;
+        }
+
         return $prop;
     }
+
 }
