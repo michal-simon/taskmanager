9e827c7537c32cd72c4978545f540698f994f8df
michael-hampton <michaelhamptondesign@yahoo.com>
2019-10-13 14:00:11 +0100

    Update TaskStatusController.php

--- app/Http/Controllers/TaskStatusController.php
+++ app/Http/Controllers/TaskStatusController.php
@@ -19,4 +19,41 @@ class TaskStatusController extends Controller
 
         return $statuses->toJson();
     }
+
+     /**
+     * Store a newly created resource in storage.
+     *
+     * @param  CreateTaskStatusRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateTaskStatusRequest $request)
+    {
+        $status = $this->taskStatuses->createTaskStatus($request->except('_token', '_method'));
+    }
+
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  UpdateTaskStatusRequest $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTaskStatusRequest $request, int $id)
+    {
+        $taskStatus = $this->taskStatuses->findTaskStatusById($id);
+        $update = new TaskStatusRepository($taskStatus);
+        $update->updateTaskStatus($request->all());
+    }
+
+     /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(int $id)
+    {
+        $this->taskStatuses->findTaskStatusById($id)->delete();
+    }
 }
