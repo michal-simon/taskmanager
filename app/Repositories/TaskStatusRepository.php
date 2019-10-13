54d8c669691073c03f3be3edd1c5ba8b7e198305
michael-hampton <michaelhamptondesign@yahoo.com>
2019-10-13 13:45:51 +0100

    Update TaskStatusRepository.php

--- app/Repositories/TaskStatusRepository.php
+++ app/Repositories/TaskStatusRepository.php
@@ -37,13 +37,73 @@ class TaskStatusRepository extends BaseRepository implements TaskStatusRepositor
     }
     
     /**
-     * @param int $id
+     * Create the order status
      *
-     * @return TaskStatus
+     * @param array $params
+     * @return OrderStatus
+     * @throws OrderStatusInvalidArgumentException
+     */
+    public function createTaskStatus(array $params) : OrderStatus
+    {
+         return $this->create($params);
+    }
+
+    /**
+     * Update the order status
+     *
+     * @param array $data
+     *
+     * @return bool
+     * @throws OrderStatusInvalidArgumentException
+     */
+    public function updateTaskStatus(array $data) : bool
+    {
+        return $this->update($data);
+    }
+
+    /**
+     * @param int $id
+     * @return OrderStatus
+     * @throws OrderStatusNotFoundException
+     */
+    public function findTaskStatusById(int $id) : OrderStatus
+    {
+        return $this->findOneOrFail($id);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function listOrderStatuses()
+    {
+        return $this->all();
+    }
+
+    /**
+     * @return bool
      * @throws \Exception
      */
-    public function findStatusById(int $id): TaskStatus {
-        return $this->findOneOrFail($id);
+    public function deleteOrderStatus() : bool
+    {
+        return $this->delete();
+    }
+
+    /**
+     * @return Collection
+     */
+    public function findTasks() : Collection
+    {
+        return $this->model->tasks()->get();
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return mixed
+     */
+    public function findByName(string $name)
+    {
+        return $this->model->where('name', $name)->first();
     }
 
 }
