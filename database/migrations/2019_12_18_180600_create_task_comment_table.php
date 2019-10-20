be394bd7a36b314afc8d5a0357b06f86c550f547
michael-hampton <michaelhamptondesign@yahoo.com>
2019-10-20 17:38:29 +0100

    Update 2019_12_18_180600_create_task_comment_table.php

--- database/migrations/2019_12_18_180600_create_task_comment_table.php
+++ database/migrations/2019_12_18_180600_create_task_comment_table.php
@@ -1,1 +1,32 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class CreateTaskCommentTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+     public function up()
+     {
+        Schema::create('task_comment', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('task_id')->unsigned();
+            $table->integer('comment_id')->unsigned();
 
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
+            $table->foreign('comment_id')->references('id')->on('comments')->onDelete('cascade');
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('invoice');
+    }
+}
