b28946863b83e837515d628e0cb18e266637572e
michael-hampton <michaelhamptondesign@yahoo.com>
2019-10-29 17:16:06 +0000

    Update and rename ContactTypeRepositoryInterface.php to CustomerTypeRepositoryInterface.php

---
+++ app/Repositories/Interfaces/CustomerTypeRepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Repositories\Interfaces;
+use App\CustomerType;
+
+interface CustomerTypeRepositoryInterface {
+    public function getAll();
+    /**
+     * 
+     * @param int $id
+     */
+    public function findCustomerTypeById(int $id): CustomerType;
+}
