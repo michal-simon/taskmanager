76fc36509bb50a2bc3b44355aa7af3221c9552e3
michael-hampton <michaelhamptondesign@yahoo.com>
2019-10-29 11:14:09 +0000

    Create CustomerTypeRepository.php

---
+++ app/Repositories/CustomerTypeRepository.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Repositories;
+use App\ContactType;
+use App\Repositories\Interfaces\ContactTypeRepositoryInterface;
+use App\Repositories\Base\BaseRepository;
+
+class SourceTypeRepository extends BaseRepository implements ContactTypeRepositoryInterface {
+    
+    /**
+     * ContactTypeRepository constructor.
+     *
+     * @param ContactType $contactType
+     */
+    public function __construct(ContactType $contactType) {
+        parent::__construct($contactType);
+        $this->model = $contactType;
+    }
+    public function getAll() {
+        return $this->model->orderBy('name', 'asc')
+                        ->get();
+    }
+    
+    /**
+     * @param int $id
+     *
+     * @return ContactType
+     * @throws \Exception
+     */
+    public function findContactType(int $id): ContactType {
+        return $this->findOneOrFail($id);
+    }
+}
