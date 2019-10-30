cd7697d15cc08328bfb11aca40783f674120c2cd
michael-hampton <michaelhamptondesign@yahoo.com>
2019-10-30 18:38:19 +0000

    Create EventTypeRepositoryInterface.php

---
+++ app/Repositories/Interfaces/EventTypeRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Repositories\Interfaces;
+use App\EventType;
+interface EventTypeRepositoryInterface {
+    public function getAll();
+    /**
+     * 
+     * @param int $id
+     */
+    public function findEventTypeById(int $id): EventType;
+}
