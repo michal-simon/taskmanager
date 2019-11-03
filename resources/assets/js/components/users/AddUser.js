09db504f7bae8cf4b9cb8dc85ea45f8a6b944bd2
michael-hampton <michaelhamptondesign@yahoo.com>
2019-11-03 10:53:34 +0000

    Change password help text

--- resources/assets/js/components/users/AddUser.js
+++ resources/assets/js/components/users/AddUser.js
@@ -301,7 +301,7 @@ class AddUser extends React.Component {
                             <Label for="password">Password:</Label>
                             <Input className={this.hasErrorFor('password') ? 'is-invalid' : ''} type="password"
                                 name="password" onChange={this.handleInput.bind(this)}/>
-                            <small className="form-text text-muted">Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.</small>
+                            <small className="form-text text-muted">Your password must be more than 8 characters long, should contain at-least 1 Uppercase, 1 Lowercase, 1 Numeric and 1 special character..</small>
                             {this.renderErrorFor('password')}
                         </FormGroup>
 
