c89f9613c5acf45abcbb5be13060563828c504f6
michael-hampton <michaelhamptondesign@yahoo.com>
2019-11-04 21:36:47 +0000

    Create TaskService.php

---
+++ app/Services/TaskService.php
@@ -0,0 +1,168 @@
+<?php
+namespace App\Services;
+use Illuminate\Http\Request;
+use App\Task;
+use App\Requests\CreateTaskRequest;
+use App\Requests\CreateDealRequest;
+use App\Requests\UpdateTaskRequest;
+use App\Repositories\Interfaces\TaskRepositoryInterface;
+use App\Repositories\TaskRepository;
+use App\Repositories\Interfaces\ProjectRepositoryInterface;
+use App\Product;
+use App\Repositories\ProductRepository;
+use App\Repositories\CustomerRepository;
+use App\Customer;
+use Illuminate\Support\Facades\Auth;
+use App\Transformations\TaskTransformable;
+use App\Repositories\SourceTypeRepository;
+use App\SourceType;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\TaskCreated;
+class TaskService extends Controller {
+    use TaskTransformable;
+    /**
+     * @var TaskRepositoryInterface
+     */
+    private $taskRepository;
+    /**
+     * @var ProjectRepositoryInterface
+     */
+    private $projectRepository;
+    /**
+     * 
+     * @param TaskRepositoryInterface $taskRepository
+     * @param ProjectRepositoryInterface $projectRepository
+     */
+    public function __construct(TaskRepositoryInterface $taskRepository, ProjectRepositoryInterface $projectRepository) {
+        $this->taskRepository = $taskRepository;
+        $this->projectRepository = $projectRepository;
+    }
+    public function list() {
+        $list = $this->taskRepository->listTasks();
+        $tasks = $list->map(function (Task $task) {
+                    return $this->transformTask($task);
+                })->all();
+        return response()->json($tasks);
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function create(CreateTaskRequest $request) {
+        $validatedData = $request->except('project_id', 'contributors');
+        $currentUser = Auth::user();
+        if (!empty($request->project_id)) {
+            $objProject = $this->projectRepository->findProjectById($request->project_id);
+        }
+        $validatedData['customer_id'] = empty($validatedData['customer_id']) && isset($objProject) ? $objProject->customer_id : $validatedData['customer_id'];
+        $validatedData['source_type'] = empty($validatedData['source_type']) ? 1 : $validatedData['source_type'];
+        $validatedData['created_by'] = $currentUser->id;
+        $task = $this->taskRepository->createTask($validatedData);
+        if ($validatedData['task_type'] == 1) {
+            $objProject->tasks()->attach($task);
+        }
+        if ($request->has('contributors')) {
+            $taskRepo = new TaskRepository($task);
+            $taskRepo->syncUsers($request->input('contributors'));
+        }
+        //send notification
+        Notification::send($currentUser, new TaskCreated($task));
+        return $task;
+    }
+    /**
+     * 
+     * @param int $task_id
+     * @return type
+     */
+    public function markAsCompleted(int $task_id) {
+        $objTask = $this->taskRepository->findTaskById($task_id);
+        $taskRepo = new TaskRepository($objTask);
+        $taskRepo->updateTask(['is_completed' => true]);
+        return true;
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id) {
+        $objTask = $this->taskRepository->findTaskById($id);
+        $taskRepo = new TaskRepository($objTask);
+        $taskRepo->deleteTask();
+        return true;
+    }
+    /**
+     * @param UpdateTaskRequest $request
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function update(UpdateTaskRequest $request, int $id) {
+        $task = $this->taskRepository->findTaskById($id);
+        $taskRepo = new TaskRepository($task);
+        $taskRepo->updateTask($request->except('contributors'));
+        if ($request->has('contributors')) {
+            $taskRepo->syncUsers($request->input('contributors'));
+        }
+
+        return true;
+    }
+    
+    /**
+     * 
+     * @param Request $request
+     * @param int $id
+     */
+    public function updateStatus(Request $request, int $id) {
+        $task = $this->taskRepository->findTaskById($id);
+        $taskRepo = new TaskRepository($task);
+        $taskRepo->updateTask(['task_status' => $request->task_status]);
+    }
+    
+
+    /**
+     * 
+     * @param CreateDealRequest $request
+     * @return type
+     */
+    public function createDeal(Request $request) {
+        $currentUser = Auth::user();
+        $userId = !$currentUser ? 9874 : $currentUser->id;
+        $request->customer_type = 2;
+        $customer = (new CustomerRepository(new Customer))->createCustomer($request->except('_token', '_method', 'valued_at', 'title', 'description'));
+        if ($request->has('address_1') && !empty($request->address_1)) {
+            $customer->addresses()->create([
+                'company_id' => $request->company_id,
+                'job_title' => $request->job_title,
+                'address_1' => $request->address_1,
+                'address_2' => $request->address_2,
+                'zip' => $request->zip,
+                'city' => $request->city,
+                'country_id' => 225,
+                'status' => 1
+            ]);
+        }
+        $task = $this->taskRepository->createTask(
+                [
+                    'created_by' => $userId,
+                    'source_type' => $request->source_type,
+                    'title' => $request->title,
+                    'description' => $request->description,
+                    'customer_id' => $customer->id,
+                    'valued_at' => $request->valued_at,
+                    'task_type' => $request->task_type,
+                    'task_status' => $request->task_status
+                ]
+        );
+
+        if ($request->has('contributors')) {
+            $taskRepo = new TaskRepository($task);
+            $taskRepo->syncUsers($request->input('contributors'));
+        }
+
+        return $task;
+    }
+}
