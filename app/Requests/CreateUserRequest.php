2d5b3dd4e9d50666df61ae7c9fbb08408618714c
michael-hampton <michaelhamptondesign@yahoo.com>
2019-11-04 08:37:55 +0000

    Update CreateUserRequest.php

--- app/Requests/CreateUserRequest.php
+++ app/Requests/CreateUserRequest.php
@@ -19,7 +19,15 @@ class CreateUserRequest extends BaseFormRequest {
             'phone_number' => 'nullable|string',
             'dob' => 'nullable|string',
             'role' => 'nullable|array',
-            'password' => 'required|string|min:6|confirmed|regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/',
+            'password' => [
+                'required',
+                'string',
+                'min:10',             // must be at least 10 characters in length
+                'regex:/[a-z]/',      // must contain at least one lowercase letter
+                'regex:/[A-Z]/',      // must contain at least one uppercase letter
+                'regex:/[0-9]/',      // must contain at least one digit
+                'regex:/[@$!%*#?&]/', // must contain a special character
+            ],
             'username' => 'required|string',
             'profile_photo' => 'nullable|string',
             'email' => 'required|string',
