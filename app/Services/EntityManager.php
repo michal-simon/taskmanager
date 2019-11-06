96c412f13fd68f4ee6a39613f2b48b978074a5ae
michael-hampton <michaelhamptondesign@yahoo.com>
2019-11-06 14:17:00 +0000

    Create EntityManager.php

---
+++ app/Services/EntityManager.php
@@ -0,0 +1,20 @@
+<?php
+class EntityManager {
+    
+    static public function getRepository($model) {
+        $r = new ReflectionObject($model);
+        $p = $r->getProperty('repository');
+        $p->setAccessible(true); // <--- you set the property to public before you read the value
+        $repositoryName = $p->getValue($model);
+        $repo = new $repositoryName($model);
+        
+        return $repo;
+    }
+}
+
+/*
+$obj = new test2();
+$repo = EntityManager::getRepository($obj);
+$testValue = $repo->test();
+var_dump($testValue);
+*/
