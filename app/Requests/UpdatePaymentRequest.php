3adcd2db407e056ab343f6e0c16ecf5498d972b7
michael-hampton <michaelhamptondesign@yahoo.com>
2019-11-10 13:35:22 +0000

    Create UpdatePaymentRequest.php

---
+++ app/Requests/UpdatePaymentRequest.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Requests;
+use App\Repositories\Base\BaseFormRequest;
+class UpdatePaymentRequest extends BaseFormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'customer_id' => ['required', 'unique:couriers'],
+            'total' => ['required'],
+            'tax_total' => ['required'],
+            'discount_total' => ['required']
+        ];
+    }
+}
