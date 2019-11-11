fc88452f8036a2ebcc346040ae584e36cbc6f562
michael-hampton <michaelhamptondesign@yahoo.com>
2019-11-11 18:51:12 +0000

    Create TaxRateUnitTest.php

---
+++ tests/Unit/TaxRateUnitTest.php
@@ -0,0 +1,92 @@
+<?php
+namespace Tests\Unit;
+use App\TaxRate;
+use App\Repositories\TaxRateRepository;
+use Tests\TestCase;
+class CourierUnitTest extends TestCase
+{
+    /** @test */
+    public function it_can_list_all_the_tax_rates()
+    {
+        $data = [
+            'name' => $this->faker->word,
+            'rate' => $this->faker->randomDecimal()
+        ];
+
+        $taxRateRepo = new TaxRateRepository(new TaxRate);
+        $taxRateRepo->createTaxRate($data);
+        $lists = $taxRateRepo->listTaxRates();
+        foreach ($lists as $list) {
+            $this->assertDatabaseHas('tax_rates', ['name' => $list->name]);
+            $this->assertDatabaseHas('tax_rates', ['rate' => $list->rate]);
+        }
+    }
+    
+    /** @test */
+    public function it_errors_when_the_tax_rate_is_not_found()
+    {
+        $this->expectException(CourierNotFoundException::class);
+        $this->expectExceptionMessage('Courier not found.');
+        $taxRateRepo = new TaxRateRepository(new TaxRate);
+        $taxRateRepo->findTaxRateById(999);
+    }
+    
+    /** @test */
+    public function it_can_get_the_tax_rate()
+    {
+        $data = [
+            'name' => $this->faker->word,
+            'rate' => $this->faker->randomDecimal()
+        ];
+
+        $taxRateRepo = new TaxRateRepository(new TaxRate);
+        $created = $taxRateRepo->createTaxRate($data);
+        $found = $taxRateRepo->findTaxRateById($created->id);
+        $this->assertEquals($data['name'], $found->name);
+        $this->assertEquals($data['rate'], $found->rate);
+    }
+    
+    /** @test */
+    public function it_errors_updating_the_tax_rate()
+    {
+        $this->expectException(CourierInvalidArgumentException::class);
+        $taxRateRepo = new TaxRateRepository($taxRate);
+        $taxRateRepo->updateTaxRate(['name' => null]);
+    }
+    
+    /** @test */
+    public function it_can_update_the_tax_rate()
+    {
+        $taxRateRepo = new TaxRateRepository($taxRate);
+        $update = [
+            'name' => $this->faker->word,
+            'rate' => $this->faker->randomDecimal(),
+        ];
+        $updated = $taxRateRepo->updateTaxRate($update);
+        $this->assertTrue($updated);
+        $this->assertEquals($update['name'], $taxRate->name);
+        $this->assertEquals($update['rate'], $taxRate->rate);
+        
+    }
+    
+    /** @test */
+    public function it_errors_when_creating_the_tax_rate()
+    {
+        $this->expectException(CourierInvalidArgumentException::class);
+        $taxRateRepo = new TaxRateRepository(new TaxRate);
+        $taxRateRepo->createTaxRate([]);
+    }
+    
+    /** @test */
+    public function it_can_create_a_tax_rate()
+    {
+        $data = [
+            'name' => $this->faker->word,
+            'rate' => $this->faker->randomDecimal(),
+        ];
+        $taxRateRepo = new TaxRateRepository(new TaxRate);
+        $created = $taxRateRepo->createTaxRate($data);
+        $this->assertEquals($data['name'], $created->name);
+        $this->assertEquals($data['rate'], $created->rate);
+    }
+}
